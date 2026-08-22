117a6cfb959e7298584e9c9b91690d46d6257157
CaioHnag <caiohnag@gmail.com>
2026-08-22 13:38:56 -0300

    Python function to summarize words

---
+++ my current projects/resumodepalavras.py
@@ -0,0 +1,20 @@
+def cript(palavra):
+    lista = []
+    a = ''
+    b = 0
+    for letra in palavra:
+        if letra != a :
+            lista.append([a , b])
+            a = letra
+            b = 1
+        else:
+            b += 1
+    lista.append([a , b])
+    c = ''
+    for nome , qt in lista[1:]:
+        c += nome
+        c += str(qt)
+    if len(c) < len(palavra):
+        return c
+    else:
+        return palavra
